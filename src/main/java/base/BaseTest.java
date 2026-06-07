4d5bc6344a2b5ebc81777b33bcad53957cf4f423
Pg4691 <piyushgautam4691@gmail.com>
2026-06-07 15:27:46 +0530

    Added first GET API test

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,4 +1,10 @@
 package base;
 
+import io.restassured.RestAssured;
+
 public class BaseTest {
+
+    public static void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
 }
--- src/test/java/tests/UserTests.java
+++ src/test/java/tests/UserTests.java
@@ -1,4 +1,32 @@
 package tests;
 
+import base.BaseTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class UserTests {
+
+    @BeforeClass
+    public void init() {
+        BaseTest.setup();
+    }
+
+    @Test
+    public void getUserTest() {
+
+        given()
+
+                .when()
+                .get("/users/1")
+
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("name", equalTo("Leanne Graham"))
+                .body("username", equalTo("Bret"));
+    }
 }
